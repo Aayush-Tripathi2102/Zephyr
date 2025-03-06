16d7d22ac16a279f1b1bed46f2cf81769408f699
Aayush-Tripathi2102 <aayushtripathi2102@gmail.com>
2025-03-06 06:15:28 +0530

    Added few more elements in the website

--- web/src/app/(auth)/login/page.tsx
+++ web/src/app/(auth)/login/page.tsx
@@ -1,9 +1,9 @@
-// pages/login.tsx
 "use client";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { login } from "@/lib/appwrite";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Login() {
   const [email, setEmail] = useState("");
@@ -25,17 +25,19 @@ export default function Login() {
   };
 
   return (
-    <aside className="w-1/2 border border-white">
-      <div className="absolute inset-0 overflow-hidden">
-        <div className="absolute -left-[300px] -top-[300px] h-[600px] w-[600px] rounded-full bg-purple-600/20 blur-[120px]" />
-        <div className="absolute -bottom-[300px] -left-[300px] h-[600px] w-[600px] rounded-full bg-purple-600/10 blur-[120px]" />
-      </div>
-      <div className="flex min-h-screen flex-col items-center justify-center p-4">
-        <div className="w-[500px] mx-auto bg-gradient-to-b from-purple-900/30 to-purple-900/10 p-8 md:p-12 rounded-3xl border border-purple-500/30 backdrop-blur-sm">
+    <div className="flex h-screen">
+      <div className="w-1/2 flex items-center justify-center relative">
+        <div className="absolute inset-0 overflow-hidden">
+          <div className="absolute -left-[300px] -top-[300px] h-[600px] w-[600px] rounded-full bg-purple-600/20 blur-[120px]" />
+          <div className="absolute -bottom-[300px] -left-[300px] h-[600px] w-[600px] rounded-full bg-purple-600/10 blur-[120px]" />
+        </div>
+        <div className="w-[500px] bg-gradient-to-b from-purple-900/30 to-purple-900/10 p-8 md:p-12 rounded-3xl border border-purple-500/30 backdrop-blur-sm">
           <h1 className="mb-6 py-2 bg-gradient-to-r from-white to-purple-200 bg-clip-text text-4xl font-bold text-transparent md:text-6xl">
             Log in
           </h1>
-          <h2 className="text-[#636363] py-2">Access your account and explore the Decentralised P2P network</h2>
+          <h2 className="text-[#636363] py-2">
+            Access your account and explore the Decentralised P2P network
+          </h2>
           <input
             type="email"
             placeholder="Email"
@@ -57,14 +59,19 @@ export default function Login() {
           >
             {loading ? "Logging in..." : "Log In"}
           </button>
-          {message && (
-            <p className="mt-2 text-center text-red-500">{message}</p>
-          )}
+          {message && <p className="mt-2 text-center text-red-500">{message}</p>}
           <p className="mt-4 text-center text-[#636363]">
-            New to the network? <Link href="/signup" className="text-purple-700 hover:underline">Sign Up</Link>
+            New to the network?{" "}
+            <Link href="/signup" className="text-purple-700 hover:underline">
+              Sign Up
+            </Link>
           </p>
         </div>
       </div>
-    </aside>
+
+      <div className="w-1/2 h-screen relative">
+        <Image src="/bg.webp" layout="fill" objectFit="cover" alt="bg" />
+      </div>
+    </div>
   );
 }
--- web/src/app/(auth)/signup/page.tsx
+++ web/src/app/(auth)/signup/page.tsx
@@ -1,9 +1,9 @@
-// pages/signup.tsx
 "use client";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { register } from "@/lib/appwrite";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Signup() {
   const [email, setEmail] = useState("");
@@ -26,53 +26,58 @@ export default function Signup() {
   };
 
   return (
-    <aside className="w-1/2 border border-white">
-      <div className="absolute inset-0 overflow-hidden">
-        <div className="absolute -left-[300px] -top-[300px] h-[600px] w-[600px] rounded-full bg-purple-600/20 blur-[120px]" />
-        <div className="absolute -bottom-[300px] -left-[300px] h-[600px] w-[600px] rounded-full bg-purple-600/10 blur-[120px]" />
-      </div>
-      <div className="flex min-h-screen flex-col items-center justify-center p-4">
-        <div className="w-[500px] mx-auto bg-gradient-to-b from-purple-900/30 to-purple-900/10 p-8 md:p-12 rounded-3xl border border-purple-500/30 backdrop-blur-sm">
-          <h1 className="mb-6 py-2 bg-gradient-to-r from-white to-purple-200 bg-clip-text text-4xl font-bold text-transparent md:text-6xl">
-            Sign up
-          </h1>
-          <h2 className="text-[#636363] py-2">Fill the form and be part of the best Decentralised P2P out there</h2>
-          <input
-            type="text"
-            placeholder="Name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            className="mb-2 w-full rounded-xl border-purple-500/30 bg-transparent border p-2 text-white placeholder:text-gray-400"
-          />
-          <input
-            type="email"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="mb-2 w-full rounded-xl border-purple-500/30 bg-transparent border p-2 text-white placeholder:text-gray-400"
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="mb-2 w-full rounded-xl border-purple-500/30 bg-transparent border p-2 text-white placeholder:text-gray-400"
-          />
-          <button
-            onClick={handleSignup}
-            disabled={loading}
-            className="w-full rounded-full mt-3 py-2 bg-purple-500/30 p-2 text-white hover:bg-purple-600/20"
-          >
-            {loading ? "Signing up..." : "Sign Up"}
-          </button>
-          {message && (
-            <p className="mt-2 text-center text-red-500">{message}</p>
-          )}
-          <p className="mt-4 text-center text-[#636363]">
-            Already part of the network? <Link href="/login" className="text-purple-700 hover:underline">Sign Up</Link>
-          </p>
+    <div className="flex">
+      <div className="w-1/2 relative">
+        <div className="absolute inset-0 overflow-hidden">
+          <div className="absolute -left-[300px] -top-[300px] h-[600px] w-[600px] rounded-full bg-purple-600/20 blur-[120px]" />
+          <div className="absolute -bottom-[300px] -left-[300px] h-[600px] w-[600px] rounded-full bg-purple-600/10 blur-[120px]" />
+        </div>
+        <div className="flex min-h-screen flex-col items-center justify-center p-4">
+          <div className="w-[500px] mx-auto bg-gradient-to-b from-purple-900/30 to-purple-900/10 p-8 md:p-12 rounded-3xl border border-purple-500/30 backdrop-blur-sm">
+            <h1 className="mb-6 py-2 bg-gradient-to-r from-white to-purple-200 bg-clip-text text-4xl font-bold text-transparent md:text-6xl">
+              Sign up
+            </h1>
+            <h2 className="text-[#636363] py-2">Fill the form and be part of the best Decentralised P2P out there</h2>
+            <input
+              type="text"
+              placeholder="Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              className="mb-2 w-full rounded-xl border-purple-500/30 bg-transparent border p-2 text-white placeholder:text-gray-400"
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="mb-2 w-full rounded-xl border-purple-500/30 bg-transparent border p-2 text-white placeholder:text-gray-400"
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="mb-2 w-full rounded-xl border-purple-500/30 bg-transparent border p-2 text-white placeholder:text-gray-400"
+            />
+            <button
+              onClick={handleSignup}
+              disabled={loading}
+              className="w-full rounded-full mt-3 py-2 bg-purple-500/30 p-2 text-white hover:bg-purple-600/20"
+            >
+              {loading ? "Signing up..." : "Sign Up"}
+            </button>
+            {message && (
+              <p className="mt-2 text-center text-red-500">{message}</p>
+            )}
+            <p className="mt-4 text-center text-[#636363]">
+              Already part of the network? <Link href="/login" className="text-purple-700 hover:underline">Log In</Link>
+            </p>
+          </div>
         </div>
       </div>
-    </aside>
+      <div className="w-1/2 h-screen relative">
+        <Image src='/bg.webp' layout="fill" objectFit="cover" alt="bg" />
+      </div>
+    </div>
   );
 }
